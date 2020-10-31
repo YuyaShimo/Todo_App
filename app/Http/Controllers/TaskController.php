9e22fcb0a34a4ecb3f4a9b1d2b7942d24c8ce98e
YuyaShimo <mei300225@gmail.com>
2020-10-31 22:20:36 +0900

    フォルダを全件取得しテンプレートへ

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+
+    // フォルダ一覧を表示
+    public function index() {
+
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+
+            'folders' => $folders,
+        ]);
+    }
 }
