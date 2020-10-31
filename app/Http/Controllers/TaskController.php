90b83a1d474e006011656ff9e59d24767216817a
YuyaShimo <mei300225@gmail.com>
2020-10-31 22:58:49 +0900

    フォルダ選択時に背景色を変更

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,13 +9,15 @@ class TaskController extends Controller
 {
 
     // フォルダ一覧を表示
-    public function index() {
+    public function index(int $id) {
 
         $folders = Folder::all();
 
         return view('tasks/index', [
 
             'folders' => $folders,
+            'current_folder_id' => $id,
+            
         ]);
     }
 }
