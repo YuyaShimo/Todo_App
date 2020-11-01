373b1b05cf4b471132af345c6003ac10fcb00a8b
YuyaShimo <mei300225@gmail.com>
2020-11-01 09:34:07 +0900

    フォルダに紐づくタスクを取得

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -13,11 +14,17 @@ class TaskController extends Controller
 
         $folders = Folder::all();
 
+        $current_folder = Folder::find($id);
+        
+        $tasks = Task::where('folder_id', '=', $current_folder->id)->get();
+        dd($tasks);
+
         return view('tasks/index', [
 
             'folders' => $folders,
-            'current_folder_id' => $id,
-            
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+
         ]);
     }
 }
