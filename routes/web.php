5a75fcbcd24e599edeb976afe70e5129a3a29e7d
YuyaShimo <mei300225@gmail.com>
2020-11-08 21:34:32 +0900

    登録画面とルーティングを定義

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
 
 Route::get('folders/{id}/tasks/', 'TaskController@index')->name('tasks.index');
 
