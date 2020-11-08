c6f973428ef7d3cff2acb217edcf39376408a733
YuyaShimo <mei300225@gmail.com>
2020-11-08 22:38:32 +0900

    Authのパッケージを取得

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Auth::routes();
-
 Route::get('folders/{id}/tasks/', 'TaskController@index')->name('tasks.index');
 
 Route::get('/folders/create/', 'FolderController@showCreateForm')->name('folders.create');
@@ -30,3 +28,7 @@ Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
