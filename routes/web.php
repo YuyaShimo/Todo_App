0b25b6f5e410064a845c857dd3d1365fb12c13c9
YuyaShimo <mei300225@gmail.com>
2020-11-08 20:45:58 +0900

    トップページへのルーティングを定義

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('folders/{id}/tasks/', 'TaskController@index')->name('tasks.index');
 
